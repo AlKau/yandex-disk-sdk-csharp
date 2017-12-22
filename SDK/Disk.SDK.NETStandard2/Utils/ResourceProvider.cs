9e4a30895548002f9f02cc18c5dd304a6b86260b
Alex <alex@finebits.com>
2017-12-22 18:31:10 +0300

    Created SDK(.Net Standard 2.0), Sample UWP project.

---
+++ SDK/Disk.SDK.NETStandard2/Utils/ResourceProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Disk.SDK.Utils
+{
+    public class ResourceProvider
+    {
+        public static string Get(string key)
+        {
+            return WebdavResources.ResourceManager.GetString(key, WebdavResources.Culture);
+        }
+    }
+}
